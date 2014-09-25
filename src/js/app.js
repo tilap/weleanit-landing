0a0ed5912c2ce6b8b015f52ce467e3f76d56f0b0
Tilap <jlavinh+bitbucket@gmail.com>
2014-09-25 22:01:48 +0200

    Google Analytics
    Google analytics with options in configuration file

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,23 @@ require('./misc/console-fix');
 require('./misc/external-links-in-new-window');
 require('./layout/home');
 require('./layout/navbar');
+
+// jshint ignore: start
+var gaConfig = require('../../config.js').app.ga;
+
+(function(i,s,o,g,r,a,m){
+    i['GoogleAnalyticsObject']=r;
+    i[r]=i[r] || function() {
+        ( 
+        i[r].q=i[r].q || []).push( arguments )},
+        i[r].l=1*new Date();
+        a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];
+        a.async=1;
+        a.src=g;
+        m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', gaConfig.id, gaConfig.name);
+ga('send', 'pageview');
+// jshint ignore: end
